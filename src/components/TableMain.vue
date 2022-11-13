267353a0e6b0aa1fe08cd804accc6ce32e94ac3a
het-t <hettarkhala068@gmail.com>
2022-11-13 18:58:08 +0530

    feature: global component of table tamplate

---
+++ src/components/TableMain.vue
@@ -0,0 +1,34 @@
+<template>
+        <div class="card">
+            <div class="card-head m0 pb16 pt16 pr16 pl16">
+                <h5 class="table-head m0">
+                    <slot name="table-heading"></slot>
+                </h5>
+                <slot name="table-action"></slot>
+            </div>
+
+            <!-- <TableFilter :tableData="rolesList" @filtered="filteredRolesList = $event" class="mr16 ml16 mt16 actions"/> -->
+
+            <div class="mr16 ml16 mt16">
+                <table>
+                    <thead>
+                        <slot name="thead"></slot>
+                    </thead>
+
+                    <tbody>
+                        <slot name="tbody"></slot>
+                    </tbody>
+                </table>
+                <!-- <TablePagination @tableData="rolesList = $event"
+                    tableName="roles"
+                /> -->
+            </div>
+        </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'TableMain'
+    }
+</script>
