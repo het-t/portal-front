e7243095a49676d61b62d6ecf75256b8dac7d742
het-t <hettarkhala068@gmail.com>
2022-11-14 16:11:08 +0530

    fix: removed unneccesary comment

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -42,46 +42,6 @@
                 </div>
             </template>
         </table-main>
-        <!-- <div class="card">
-            <div class="card-head m0 pb16 pt16 pr16 pl16">
-                <h5 class="table-head m0">Tasks</h5>
-                <router-link to="/u/tasks/create-task" class="table-action">
-                    create task
-                </router-link>
-            </div>
-
-            <div class="mr16 ml16 mt16">
-                <table>
-                    <tr class="table-heading">
-                        <th>title</th>
-                        <th>client</th>
-                        <th>progress</th>
-                        <th>status</th>
-                    </tr>
-
-                    <div v-for="(task, index) of tasksList" :key="task.taskId">
-                        <tr class="tr">
-                            <td class="flex">
-                                <div class="dots">
-                                    <img @click="editTask('row'+index)" src="../assets/icons/dots-icon.png" alt="" class="dots">
-                                </div>
-                                {{task.taskTitle}}
-                            </td>
-                            <td>{{task.taskClient}}</td>
-                            <td>
-                                <tasks-progress/>
-                            </td>
-                            <td>{{task.taskStatus}}</td>
-                        </tr>
-
-                        <tr class="tr tr-hidden hide mb16" :ref="'row'+index">
-                            <tasks-create :taskId="task.taskId" displayHead='false' :uk="index" class="tasks-create"/>
-                        </tr>
-                    </div>
-
-                </table>
-            </div>
-        </div> -->
     </div>
 </template>
 
@@ -102,19 +62,7 @@ import { getTasks } from '@/api/index.js';
         getTasks()
         .then(tasks => {
             this.tasksList = tasks.data
-            console.log(tasks.data)
         })
-        // this.tasksList = [{
-        //         taskId: 26,
-        //         taskTitle: "Incorporation of Company",
-        //         taskClient: "TechAvidus",
-        //         taskStatus: "pending"
-        //     }, {
-        //         taskId: 29,
-        //         taskTitle: "Income Tax return",
-        //         taskClient: "TechSome",
-        //         taskStatus: "done"
-        //     }];
     },
     methods: {
         editTask(rowIndex) {
