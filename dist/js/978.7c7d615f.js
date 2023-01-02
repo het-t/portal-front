d4a5f4666ea6ab1259a0824b09247cd37af8d79c
het-t <hettarkhala068@gmail.com>
2023-01-02 17:39:26 +0530

    Initial dist subtree commit

---
+++ dist/js/978.7c7d615f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportal"]=self["webpackChunkportal"]||[]).push([[978],{5978:function(n,t,e){e.r(t),e.d(t,{default:function(){return s}});var r=e(6252);function u(n,t){return(0,r.wg)(),(0,r.iD)("div",null," you dont have access to this page ")}var a=e(3744);const c={},o=(0,a.Z)(c,[["render",u]]);var s=o}}]);
+//# sourceMappingURL=978.7c7d615f.js.map
